ca40de98e6b9cced8be0915f6d49804aa687680b
mobin <mobinyazdanparast9@gmail.com>
2022-07-09 11:17:43 +0430

    add mysql dumper

---
+++ database/mysql/mysql.go
@@ -0,0 +1,45 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+type MysqlConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+
+func Dump(cfg *MysqlConfig) (filePath string, filename string, err error) {
+	dumpDir := "tmp_dumps"
+	dumpFilenameFormat := fmt.Sprintf("%s-2006-01-02 15:04:05", cfg.Database)
+
+	if _, err := os.Stat(dumpDir); os.IsNotExist(err) {
+		if err := os.Mkdir(dumpDir, os.ModePerm); err != nil {
+			return "", "", err
+		}
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.User))
+	if err != nil {
+		return "", "", err
+	}
+
+	dmp, err := register(db, dumpDir, dumpFilenameFormat)
+	if err != nil {
+		return "", "", err
+	}
+
+	fullPath, filename, err := dmp.dumper()
+	if err != nil {
+		return "", "", err
+	}
+
+	dmp.Close()
+
+	return fullPath, filename, nil
+}
