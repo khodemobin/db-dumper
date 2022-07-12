a2710055547162d0d9d52dbb7657f5e73adb182e
mobin <mobinyazdanparast9@gmail.com>
2022-07-12 10:58:28 +0430

    add sftp driver

--- storage/sftp.go
+++ storage/sftp.go
@@ -3,7 +3,7 @@ package storage
 import (
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
-	"log"
+	"io/ioutil"
 )
 
 type SftpConfig struct {
@@ -16,6 +16,37 @@ type SftpConfig struct {
 }
 
 func sftpLocal(path string, fileName string, cfg *SftpConfig) error {
+	client, err := sftpConnect(cfg)
+	if err != nil {
+		return err
+	}
+
+	file, err := client.Create(cfg.UploadPath + "/" + fileName)
+	if err != nil {
+		return err
+	}
+
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(input); err != nil {
+		return err
+	}
+
+	if err := file.Close(); err != nil {
+		return err
+	}
+
+	if err := client.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sftpConnect(cfg *SftpConfig) (*sftp.Client, error) {
 	config := &ssh.ClientConfig{
 		User: cfg.User,
 		Auth: []ssh.AuthMethod{
@@ -25,42 +56,9 @@ func sftpLocal(path string, fileName string, cfg *SftpConfig) error {
 	}
 
 	conn, err := ssh.Dial("tcp", cfg.Host+":"+cfg.Port, config)
-	client, err := sftp.NewClient(conn)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	// walk a directory
-	w := client.Walk(cfg.UploadPath)
-	for w.Step() {
-		if w.Err() != nil {
-			continue
-		}
-		log.Println(w.Path())
-	}
-
-	f, err := client.Create(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := f.Write([]byte("Hello world!")); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := f.Close(); err != nil {
-		return err
-	}
-
-	fi, err := client.Lstat("hello.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(fi)
-
-	if err := client.Close(); err != nil {
-		return err
-	}
-
-	return nil
+	return sftp.NewClient(conn)
 }
